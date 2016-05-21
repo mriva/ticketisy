60ae2b0eb279ce66404ebd40c8ff3414a4c7dac5
Matteo Riva <muradin@gmail.com>
2016-05-22 00:59:16 +0200

    Filtri e JOIN su departments in TicketCollection

--- app/Collections/TicketCollection.php
+++ app/Collections/TicketCollection.php
@@ -3,20 +3,28 @@
 namespace App\Collections;
 
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class TicketCollection extends RestCollection {
     
     protected $local_actions = [
-        'user' => 'filterUser',
-        'service' => 'filterService',
-        'status' => 'filterStatus',
+        'user'       => 'filterUser',
+        'service'    => 'filterService',
+        'status'     => 'filterStatus',
+        'technician' => 'filterTechnician',
     ];
 
     public function __construct() {
         $this->resource = DB::table('tickets')
-            ->select('tickets.*', 'services.name AS service_name', 'products.title AS product_title')
+            ->select([
+                'tickets.*',
+                'services.name AS service_name',
+                'products.title AS product_title',
+                'departments.name AS department',
+            ])
             ->join('services', 'tickets.service_id', '=', 'services.id')
-            ->join('products', 'services.product_id', '=', 'products.id');
+            ->join('products', 'services.product_id', '=', 'products.id')
+            ->join('departments', 'tickets.department_id', '=', 'departments.id');
 
         $this->actions = array_merge($this->actions, $this->local_actions);
     }
@@ -44,6 +52,13 @@ class TicketCollection extends RestCollection {
     public function filterStatus($status) {
         $values = explode(',', $status);
         $this->resource = $this->resource->whereIn('status', $values);
+    }
+
+    public function filterTechnician($value) {
+        if ($value == 'me') {
+            $user = Auth::guard('api')->user();
+            $this->resource = $this->resource->where('technician_id', $user->id);
+        }
     }
 
 }
